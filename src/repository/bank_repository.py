e43348234f6decf24e0b102f3fa2ea2db75df67d
Victorino Fernández <victorino_fdez@icloud.com>
2025-12-17 13:02:36 +0100

    gestion bancaria sin validaciones v1

--- src/repository/bank_repository.py
+++ src/repository/bank_repository.py
@@ -2,7 +2,6 @@ import json
 
 DATA_FILE = "data.json"
 
-
 def cargar_datos():
     try:
         with open(DATA_FILE, "r", encoding="utf-8") as f:
@@ -17,18 +16,46 @@ def guardar_datos(data):
     with open(DATA_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
-
 def login(data, pin):
     for cuenta in data["cuentas"].values():
         if str(cuenta.get("pin")) == str(pin):
             return cuenta
     return None
 
-
 def cuentas_de_usuario(data, id_usuario):
     return [c for c in data["cuentas"].values() if c.get("id_usuario") == id_usuario]
 
 # Añade una cantidad de dinero a la cuenta que se le pasa
 def deposit_money(data, account, quantity):
-    account["saldo"] += quantity
+    account["balance"] += quantity
     guardar_datos(data)
+
+
+def get_user_by_dni(dni,data):
+    for user in data["users"].values():
+        if dni == user["dni"]:
+            return user
+    return None
+
+def get_account_by_id (id,data):
+    account_list = []
+    for account in data["accounts"].values():
+        if id == account["id_user"]:
+            account_list.append(account)
+    return account_list
+
+def get_balance(account):
+    return account['balance']
+
+def get_account_number(account):
+    return account["account_number"]
+
+def select_account(accounts, selected_account):
+    return accounts[selected_account - 1]
+
+def withdraw_cash(data, account, quantity):
+    account["balance"] -= quantity
+    guardar_datos(data)
+
+def get_user_name(user):
+    return user["name"] + " " + user["surname"]
